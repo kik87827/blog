817f1d43a990be30e3294c9bf47eb1437ee417d5
kik8727 <kik87827@gmail.com>
2025-02-26 01:30:37 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container mt-4" style="text-align: center">
+    <h5>React 개발자의 블로그입니다</h5>
+    <p>- Vue로 만들었음 -</p>
+  </div>
+
+  <ArticleList :blogArticle="blogArticle" />
+</template>
+
+<script>
+import ArticleList from './components/ArticleList.vue'
+import blogData from './assets/blogData.js'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      blogArticle: blogData,
+    }
+  },
+  components: { ArticleList },
+}
+</script>
+
+<style></style>
---
+++ src/components/ArticleList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="text-align: center">
+    <div v-for="(item, index) in blogArticle" :key="index">
+      <h5>{{ item.title }}</h5>
+      <p>{{ item.date }}</p>
+    </div>
+    <!-- <div>
+      <h5>블로그 글 제목</h5>
+      <p>블로그 글 날짜</p>
+    </div>
+    <div>
+      <h5>블로그 글 제목</h5>
+      <p>블로그 글 날짜</p>
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArticleList',
+  props: {
+    blogArticle: Array,
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
